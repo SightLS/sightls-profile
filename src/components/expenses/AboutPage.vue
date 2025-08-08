2036ef4b3c1b47bb76b05a77d09682e8d4b1931c
NURULLAEV Ramil <Ramil.NURULLAEV@corphn.com>
2025-08-08 11:19:39 +0300

    Вернул описание к проекту

--- src/components/expenses/AboutPage.vue
+++ src/components/expenses/AboutPage.vue
@@ -1,21 +1,16 @@
 <template>
   <div> <!-- Это теперь единственный корневой элемент -->
     <div class="about">
-      <p>К сожалению, БД, которое я делал в 2021 на superbase пропало...</p>
+      <p>К сожалению, БД, которое я делал в 2021 на superbase пропало. Сейчас все заполненно рандомными данными прямо на фронте, но тут осталось классное модальное окно. В данном приложение я использовал vue-router, vuex, так же написал отдельный плагин для модального окна и создал кастомную дерикторию для реализации v-click-outside.</p>
     </div>
-    
-    <FightMain/>
   </div>
 </template>
 
 <script>
 
-import FightMain from '../fighting/FightMain.vue';
 export default {
   name: 'AboutPage',
-  components: {
-    FightMain
-  }
+  components: {}
 }
 </script>
 
